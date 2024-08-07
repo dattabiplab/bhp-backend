930b0e38c0e94835858ce16b5620a914d03e3dc2
Biplab Datta <dattabiplab021@gmail.com>
2024-08-07 21:47:43 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, jsonify
+import json
+import pickle
+import numpy as np
+import os
+
+app = Flask(__name__)
+
+__locations = None
+__data_columns = None
+__model = None
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+@app.route('/predict_home_price',methods=['GET','POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+    response = jsonify({
+        'estimated_price': get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+
+def get_estimated_price(location, sqft, bhk, bath):
+    global __data_columns
+    global __model
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return round(__model.predict([x])[0], 2)
+
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
+    global __model
+
+    try:
+        with open("./artifacts/columns.json", "r") as f:
+            __data_columns = json.load(f)['data_columns']
+            __locations = __data_columns[3:]
+        # print(f"Data columns: {__data_columns}")
+
+        if __model is None:
+            with open("./artifacts/bangalore_home_prices_model.pickle", 'rb') as f:
+                __model = pickle.load(f)
+        print("loading saved artifacts...done")
+    except Exception as e:
+        print(f"Error loading artifacts: {e}")
+
+def get_location_names():
+    global __locations
+    return __locations
+
+def get_data_columns():
+    global __data_columns
+    return __data_columns
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    load_saved_artifacts()
+    app.run()
